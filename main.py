9533994bc9e7b5918c78b427f45a7f6bf65a9955
Sasa-00 <miljatovicsasa9@gmail.com>
2022-04-23 12:38:28 +0200

    Dodavanje komentara

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import data
 import random
 
 
+# Funkcija nasumiscnog uzimanje pitanja iz baze "data.py"
 def generisanje_objekata():
     broj = random.randint(0, len(data.data))
     objekat = data.data[broj]
     return f"{objekat['name']}, a {objekat['description']}, from {objekat['country']}.", int(objekat['follower_count'])
 
 
+# Funkcija provere odgovora u zavisnosti od unosa
 def uporedjivanje_pratioca(foloversi1, foloversi2, slovo):
     if slovo == "A":
         if foloversi1 > foloversi2:
@@ -21,6 +23,7 @@ def uporedjivanje_pratioca(foloversi1, foloversi2, slovo):
             return False
 
 
+# U promenljivu "takmicar1" se smesta funkcija "generisanje_objekata()"
 takmicar1 = generisanje_objekata()
 run = True
 i = 0
@@ -28,16 +31,22 @@ print("\nDOBRO DOSLI U IGRU 'HIGHER LOWER'")
 
 
 while run:
+    # U promenljivu "takmicar2" se smesta funkcija "generisanje_objekata()"
     takmicar2 = generisanje_objekata()
 
     print(f"\n///////////////////////////////////////////////////////////////////////////////\nTRENUTNI SKOR = {i}")
+    # Ispisivanje stringa koji se nalazi na prvom mestu u n-torci return-a funkcije "generisanje_objekata"
     print(f"\nCompare A: {takmicar1[0]}")
     print("VS")
     print(f"Compare B: {takmicar2[0]}")
     slovo = input("Ko ima vise foloversa? Upisi 'A' ili 'B': ")
 
+    # Pozivanje funkcije "uporedjivanje_pratioca" radi provere tacnog unosa
+    # Uzima se drugo mesto u n-torci return-a funkcije "generisanje_objekata"
     isTrue = uporedjivanje_pratioca(takmicar1[1], takmicar2[1], slovo)
 
+    # U odnosu na return funkcije "uporedjivanje_pratioca", dodaje se poen i prelazi na vrh while petlje, ili
+    # se izlazi iz igrice u slucaju netacnog odgovora
     if isTrue != False:
         if isTrue == takmicar1[1]:
             takmicar1 = takmicar1
